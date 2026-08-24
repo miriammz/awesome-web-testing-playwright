8cf64c55aef0697e6b1eb9bd26be80bd42c29244
Miriam <mirimz21@hotmail.com>
2026-08-24 16:58:15 +0200

    tutorial 05, se modifica la estructura, se agrega describe y varios tests independientes, no uno general

--- app/tests/trello.spec.ts
+++ app/tests/trello.spec.ts
@@ -1,30 +1,45 @@
 import {test, expect } from './fixtures/trello-test';
 
-test.beforeAll(async ({ request }) => {
-    // Clear the database
-    await request.post('http://localhost:3000/api/reset');
-});
+test.describe('Trello-like board', () => {
+    const boardName = 'Chores';
+    const listName = 'TO DO';
 
-test('Create a new board with a list and cards', async ({ getStartedPage, myBoardsPage, boardPage }) => {
-    // load the app
-    await getStartedPage.load();
-    // create a new board
-    await getStartedPage.createFirstBoard('Chores');
-    //verify the page appears as expected
-    await boardPage.expectNewBoardLoaded('Chores');
-    // create a new list
-    await boardPage.addList('TO DO');
-    //verify the list was created
-    await expect(boardPage.listName).toHaveValue('TO DO');
-    //add cards to the list
-    await boardPage.addCardToList(0, 'Buy groceries');
-    await boardPage.addCardToList(0, 'Walk the dog');
-    await boardPage.addCardToList(0, 'Mow the lawn');
-    //verify the three cards appear
-    await expect(boardPage.cardTexts).toHaveText(
-        ['Buy groceries', 'Walk the dog', 'Mow the lawn']);
-    //navigate to the home page
-    await boardPage.goHome();
-    //verify the home page appears as expected
-    await myBoardsPage.expectLoaded(['Chores']);
-});
+    test.beforeEach(async ({ request, getStartedPage }) => {
+        // Clear the database
+        await request.post('http://localhost:3000/api/reset');
+        // load the app
+        await getStartedPage.load();
+        // create a new board
+        await getStartedPage.createFirstBoard(boardName);
+    });
+
+    test('should create the first board', async ({ boardPage }) => {
+        //verify the page appears as expected
+        await boardPage.expectNewBoardLoaded(boardName);
+    });
+
+    test('should create the first list in a board', async ({ boardPage }) => {
+        // create a new list
+        await boardPage.addList(listName);
+        //verify the list was created
+        await expect(boardPage.listName).toHaveValue(listName);
+    });
+
+    test('should create a list with multiple cards', async ({ boardPage }) => {
+        //add cards to the list
+        await boardPage.addList(listName);
+        await boardPage.addCardToList(0, 'Buy groceries');
+        await boardPage.addCardToList(0, 'Walk the dog');
+        await boardPage.addCardToList(0, 'Mow the lawn');
+        //verify the three cards appear
+        await expect(boardPage.cardTexts).toHaveText(
+            ['Buy groceries', 'Walk the dog', 'Mow the lawn']);
+    });
+
+    test('should navigate home from a board', async({ boardPage, myBoardsPage }) => {
+        //navigate to the home page
+        await boardPage.goHome();
+        //verify the home page appears as expected
+        await myBoardsPage.expectLoaded([boardName]);
+    });
+})
